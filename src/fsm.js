16e2304234ebca5b87a5788104d2ba53a47f6710
Sergey Skakun <thexfiles116@gmail.com>
2017-02-05 23:44:32 +0300

    finitie-state-mashine/new-feature constructor and getState()

--- src/fsm.js
+++ src/fsm.js
@@ -3,19 +3,29 @@ class FSM {
      * Creates new FSM instance.
      * @param config
      */
-    constructor(config) {}
+    constructor(config) {
+        this.activeState = config.initial;
+        this.states = config.states;
+
+        return this.activeState;
+    }
 
     /**
      * Returns active state.
      * @returns {String}
      */
-    getState() {}
+    getState() {
+        return this.activeState;
+    }
 
     /**
      * Goes to specified state.
      * @param state
      */
-    changeState(state) {}
+    changeState(state) {
+
+
+    }
 
     /**
      * Changes state according to event transition rules.
